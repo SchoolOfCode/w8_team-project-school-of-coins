8ca8d3e20d399e2edb7b0da61e095833e0d934ab
Jack Browne <jackbrowne00@gmail.com>
2021-05-20 11:33:18 +0100

    basic fetch

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 console.log("JS connected to HTML")
+
+let newCard;
+let deckID;
+
+let  waitForDeckID = false;
+
+const card = document.getElementById("card-text");
+
+async function newDeck(){
+    waitForDeckID = true;
+const response = await fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=2");
+newCard = await response.json();
+console.log(newCard)
+card.innerHTML = `${newCard.cards[0].code} + ${newCard.cards[1].code}`
+deckID = newCard.deck_id
+console.log(deckID)
+    waitForDeckID = false;
+}
+
+newDeck();
+
+
+async function drawCard(){
+    const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`)
+    const drawCard = await response.json();
+    console.log(drawCard);
+}
+
