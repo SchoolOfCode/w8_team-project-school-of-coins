07177ba904d6ab29f791e44446180bb580524a1b
bit-modulator <neurapraxia@gmail.com>
2021-05-20 19:53:39 +0100

    ACE logic, win logic and reset button created

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ const drawCardBtn = document.querySelector("#draw-card");
 const playerScoreDisplayElem = document.querySelector("#player-score");
 const computerScoreDisplayElem = document.querySelector("#computer-score");
 const standBtn = document.querySelector("#stand");
+const resetBtn = document.querySelector("#reset");
 
 startGameBtn.addEventListener("click", startGame);
 drawCardBtn.addEventListener("click", function(){
     drawCard(user);
 });
 standBtn.addEventListener("click", stand);
+resetBtn.addEventListener("click", resetBoard);
 
 class Player {
     constructor(id="computer",username="The House", hand=[],softHand=false,score=0, balance=100000,showHiddenCard = false,cardNum=0){
@@ -53,24 +55,37 @@ class Player {
         let parentDivElem = document.querySelector(`#${this.id}-score`);
         parentDivElem.innerHTML = `<h2>${this.username}s score is: ${this.score}</h2>`;
     }
+    reset(){
+        let parentImgDiv = document.querySelector(`#${this.id}-cards`);
+        let parentScoreDiv = document.querySelector(`#${this.id}-score`);
+        while (parentImgDiv.firstChild) {
+            parentImgDiv.removeChild(parentImgDiv.lastChild);
+        }
+        parentScoreDiv.innerHTML="";
+        this.hand = [];
+        this.score = 0;
+        this.showHiddenCard = false;
+        this.softHand=false;
+    }
 }
 
 let user = new Player("player","Emilio");
 let computer = new Player();
 let fetchCardErrorCount = 0;
 const royals = ["KING", "JACK", "QUEEN"];
-let deckID = "amkpqcyydwa0"; 
+let deckID //= "x8ezqo789qmx"; 
 //to prevent creating a new deck every time during testing- set this to the deckID
 
 async function getDecks(){
     const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6");
     const newDecks = await response.json();
-    // deckID = await newDecks.deck_id;
-    // console.log(deckID)
+    deckID = await newDecks.deck_id;
+    console.log(deckID)
 }
 
 async function drawCard(player){
-    const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
+    const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`,{
+        method: 'GET', headers: {'Content-Type': 'application/json'}});
     if (response.status !== 200) {
         fetchCardErrorCount++;
         if (fetchCardErrorCount > 3) throw "Unable to get any cards!!";
@@ -80,6 +95,10 @@ async function drawCard(player){
     const drawnCard = await response.json();
     player["hand"].push(drawnCard);
     player.updateScore(drawnCard.cards[0]);
+    if (player.softHand && player.score > 21 && player.score <= 31){
+        player.score -= 10; //convert the ace from 11 to 1
+        console.log("removed")
+    }
     player.displayHand(player);
     player.displayScore();
     player.cardNum++;
@@ -90,23 +109,38 @@ function startGame(){
     drawCard(computer);
     drawCard(user);
     drawCard(user);
-
 }
 
 async function stand(){
     computer.showHiddenCard = true;
     await drawCard(computer);
-    for (let i=computer.hand.length; i<6; i++){
-        if (computer.score >= 16) break;
+    while (computer.score < 17 && computer.hand.length <6){
         await drawCard(computer);
-        console.log(computer.score)
     }
-    //The dealer then reveals the hidden card and must draw cards
-    //one by one, until the cards total up to 17 points. 
-    //At 17 points or higher the dealer must stop.
-
-
+    // See https://en.wikipedia.org/wiki/Blackjack#Rules
+    /*
+    If the player is dealt an Ace and a ten-value card (called a "blackjack" or "natural"), and the dealer does not, the player wins and usually receives a bonus.
+    If the player exceeds a sum of 21 ("busts"), the player loses, even if the dealer also exceeds 21.
+    If the dealer exceeds 21 ("busts") and the player does not, the player wins.
+    If the player attains a final sum higher than the dealer and does not bust, the player wins.
+    If both dealer and player receive a blackjack or any other hands with the same sum, this will be called a "push" and no one wins.
+   */
+    if (user.softHand && user.cardNum === 2 && user.score ===21){
+        console.log("Blackjack! Natural. You get a bonus!!!");
+    } else if(user.score > 21){
+        console.log("Bust!!!");
+    } else if (computer.score > 21 && user.score <= 21){
+        console.log("You win!");
+    } else if(user.score > computer.score && user.score <= 21){
+        console.log("You win!");
+    } else{
+        console.log("It's a draw");
+    }
 }
 
+function resetBoard(){
+    user.reset();
+    computer.reset();
+}
 
 window.onload = getDecks();
