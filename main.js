56674fb12caeb4f6b6422597e2dc8c53f73b0a12
bit-modulator <neurapraxia@gmail.com>
2021-05-21 15:02:17 +0100

    comments added, fixed a bug with the deck ID and stand button

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
+// DOM Selectors #######################################################
 const playerBalanceDisplay = document.querySelector("#player-balance");
-
 const playerCardElem = document.querySelector("#player-cards");
 const playerScoreDisplayElem = document.querySelector("#player-score");
-
 const computerCardElem = document.querySelector("#computer-cards");
 const computerScoreDisplayElem = document.querySelector("#computer-score");
-
 const outcomeDisplayElem = document.querySelector("#game-outcome");
-
 const startGameBtn = document.querySelector("#start-game");
 const drawCardBtn = document.querySelector("#draw-card");
 const standBtn = document.querySelector("#stand");
 const resetBtn = document.querySelector("#reset");
 
-
+// Event Handlers #######################################################
 startGameBtn.addEventListener("click", startGame);
 drawCardBtn.addEventListener("click", function(){
     drawCard(user);
@@ -21,7 +18,15 @@ drawCardBtn.addEventListener("click", function(){
 standBtn.addEventListener("click", stand);
 resetBtn.addEventListener("click", resetBoard);
 
+// Class Decleration #######################################################
 class Player {
+
+    /*#################################################################
+    Default values are applied when we don't supply any below when we make 
+    a new user or computer; are for the house, override for any created users 
+    on new class instantitation e.g. new Player(id="Jack",username="cryptoKing")
+    #################################################################*/
+
     constructor(id="computer",username="The House",balance=100000,hand=[],softHand=false,score=0,showHiddenCard=false,cardNum=0){
         this.id = id;
         this.username = username;
@@ -32,8 +37,14 @@ class Player {
         this.showHiddenCard = showHiddenCard;
         this.cardNum = cardNum;
     }
+
     updateScore(card){
-        if (royals.includes(card.value)){
+    /*#################################################################
+    If the value of the card is a royal [Q,J,K] then increase score by 10
+    If the card is an ACE, default to 11, set softHand variable to true
+    Otherwise the value of the card will be added to the score
+    #################################################################*/
+        if (ROYALS.includes(card.value)){
             this.score += 10;
         } else if (card.value === "ACE"){
             this.score += 11;
@@ -42,14 +53,23 @@ class Player {
             this.score += parseInt(card.value,10);
         }
     }
-    displayHand(player){
+    displayHand(playerObj){
+    /*#################################################################
+    Select the appropriate div based on the players id i.e. computer or user
+    If we wish to show what is under the hidden card i.e. showHiddenCard = true, 
+    hide the back of the card.
+    Otherwise, create an img element for each drawn card and append it to the
+    appropriate div container e.g. computer-cards or user-cards
+    If we are the computer, and we wish to hide the second card initially, 
+    insert a blank card i.e. the back of the card image as a placeholder
+    #################################################################*/
         let parentDivElem = document.querySelector(`#${this.id}-cards`);
-        if (this.showHiddenCard){
+        if (this.showHiddenCard===true){
             let hiddenCard = document.querySelector("#hidden-card");
             hiddenCard.style.display = "none";
         }
         let imgElem = document.createElement("img");
-        imgElem.src=player.hand[this.cardNum].cards[0].image;
+        imgElem.src=playerObj.hand[this.cardNum].cards[0].image;
         parentDivElem.appendChild(imgElem);
         if (this.id ==="computer" && this.showHiddenCard == false){
             let hiddenCard = document.createElement("img");
@@ -59,10 +79,16 @@ class Player {
         }
     }
     displayScore(){
+    /*#################################################################
+    Change the value of the appropriate user/computer element with the score
+    #################################################################*/
         let parentDivElem = document.querySelector(`#${this.id}-score`);
         parentDivElem.innerHTML = `<h2>${this.username}'s score is: ${this.score}</h2>`;
     }
     reset(){
+    /*#################################################################
+    Clear all images, clear the game outcome, clear the hand, score etc
+    #################################################################*/
         let parentImgDiv = document.querySelector(`#${this.id}-cards`);
         let parentScoreDiv = document.querySelector(`#${this.id}-score`);
         outcomeDisplayElem.innerHTML = "";
@@ -78,48 +104,85 @@ class Player {
     }
 }
 
+// Class Instantiation #####################################################
 let user = new Player("player","Emilio",5000);
 let computer = new Player();
 
-let fetchCardErrorCount = 0;
-const royals = ["KING", "JACK", "QUEEN"];
-let deckID //= "vtq1bnblc5we"; 
-//to prevent creating a new deck every time during testing- set this to the deckID
+// Global Variables #######################################################
 const TEMP_BET = 1000;
+const ROYALS = ["KING", "JACK", "QUEEN"];
+const DECKS_TO_FETCH = 6;
+
+let remainingCardsInDeck = 0;
+let deckID;
+let fetchCardErrorCount = 0;
+
+
+// Functions #############################################################
 
 async function getDecks(){
-    const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6");
+    /*#################################################################
+    Fetch the stack of 6 decks
+    #################################################################*/
+    const response = await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${DECKS_TO_FETCH}`);
     const newDecks = await response.json();
     deckID = await newDecks.deck_id;
-    console.log(deckID)
+    remainingCards = 52*DECKS_TO_FETCH;
 }
 
-async function drawCard(player){
+async function drawCard(playerObj){
+    /*#################################################################
+    Take in a player object e.g. user or computer so we know which object
+    to add the card and card values etc to
+    Draw a single card, there are some errors with the server side JSON
+    so if we get a HTTP 500 return value, try again to a maximum of 5 times
+    TODO: Implement a way to notify the user to try refreshing the page
+    #################################################################*/
     const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`,{
         method: 'GET', headers: {'Content-Type': 'application/json'}});
     if (response.status !== 200) {
         fetchCardErrorCount++;
-        if (fetchCardErrorCount > 3) throw "Unable to get any cards!!";
-        drawCard(player);
+        if (fetchCardErrorCount > 5) throw "Unable to get any cards!!";
+        drawCard(playerObj);
         console.error(response.status,fetchCardErrorCount,"Oops! Let's try that again!")
     }
     const drawnCard = await response.json();
-    player["hand"].push(drawnCard);
-    player.updateScore(drawnCard.cards[0]);
-    if (player.softHand && player.score > 21 && player.score <= 31){
-        player.score -= 10; //convert the ace from 11 to 1
-        player.softHand = false; //prevent it from always taking away 10 if an ace is in the hand
+    /*#################################################################
+    Add the card to the hand of the current player, update the score
+    utilising the playerObj updatescore method. Implement automatic softhand
+    compensation - ACE will change to 1 if the drawn card makes the score >21
+    If the score is already >31, then there is no point changing the score
+    base on the ACE as the user has lost regardless
+    #################################################################*/
+    playerObj["hand"].push(drawnCard);
+    playerObj.updateScore(drawnCard.cards[0]);
+    if (playerObj.softHand && playerObj.score > 21 && playerObj.score <= 31){
+        playerObj.score -= 10;          //convert the ace from 11 to 1
+        playerObj.softHand = false;     //prevent it from always taking away 10 if an ace is in the hand
     }
-    player.displayHand(player);
-    player.displayScore();
-    player.cardNum++;
+    /*#################################################################
+    Use the player objects display hand method to display the cards on the 
+    screen. Update the displayed score, increase the total card count, 
+    reset the error count if a card is sucessfully drawn, subtract one 
+    from our total deck size as we have drawn a card
+    #################################################################*/
+    playerObj.displayHand(playerObj);
+    playerObj.displayScore();
+    playerObj.cardNum++;
     fetchCardErrorCount = 0;
+    remainingCardsInDeck--;
 }
 
 async function stand(){
+    /*#################################################################
+    Hide the hidden card overlay (the back of the card), and draw another card 
+    for the computer. Keep drawing cards for the house, until we have a 
+    maximum of 6 cards or the score exceeds 21. Then check the winning
+    conditions and display the outcome and change in balance.
+    #################################################################*/
     computer.showHiddenCard = true;
     await drawCard(computer);
-    while (computer.score < 17 && computer.hand.length <6){
+    while (computer.score < 17 && computer.cardNum <6){
         await drawCard(computer);
     }
     // See https://en.wikipedia.org/wiki/Blackjack#Rules
@@ -143,22 +206,28 @@ async function stand(){
     }
     playerBalanceDisplay.innerText = `${user.username}'s balance is: ${user.balance}`;
 }
-//
+
 function resetBoard(){
+    /*#################################################################
+    clear the screen and reset everything apart from the balance
+    #################################################################*/
     user.reset();
     computer.reset();
 }
 
-function startGame(){
+async function startGame(){
+    /*#################################################################
+    Re-use the same deck unless there are less than 12ish cards remaining,
+    otherwise get a new deck, just to be safe. Draw once for the computer,
+    twice for the user
+    #################################################################*/
+    if (remainingCardsInDeck <= 15){
+        await getDecks();
+    }
     playerBalanceDisplay.innerText = `${user.username}'s balance is: ${user.balance}`;
     drawCard(computer);
     drawCard(user);
     drawCard(user);
-}
-
-function stand(){
-    computer.showHiddenCard = true;
-    drawCard(computer);
 }
 
 //Javascript needed to turn on and off the overlay effect
@@ -169,4 +238,3 @@ function overlayOn(){
 function overlayOff(){
     document.getElementById("overlay").style.display = "none";
 }
-window.onload = getDecks();
