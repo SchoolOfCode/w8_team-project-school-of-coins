23e17701047c05a6eefdb86dd574229c635b5f1e
Lewis Murray <lewisdmurray00@gmail.com>
2021-05-22 11:52:19 +0100

    More css added to stake buttons

--- main.js
+++ main.js
@@ -280,7 +280,7 @@ async function stand(computer,user){
         outcomeDisplayElem.innerHTML = "<h2>You lose !</h2>";
         user.balance -= TEMP_BET;
     } else {
-        outcomeDisplayElem.i nnerHTML = "<h2>It's a draw</h2>";
+        outcomeDisplayElem.innerHTML = "<h2>It's a draw</h2>";
     }
     playerBalanceDisplay.innerText = `${user.username}'s balance is: ${user.balance}`;
     console.log(blackjackPlayers);
